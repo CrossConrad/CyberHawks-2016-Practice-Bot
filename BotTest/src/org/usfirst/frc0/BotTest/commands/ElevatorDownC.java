847e1607d075b7c1667df31ff7735690bf7846d1
Rohan Vasavada <rohanvasavada@gmail.com>
2016-01-30 14:44:52 -0600

    Finished elevator

--- BotTest/src/org/usfirst/frc0/BotTest/commands/ElevatorDownC.java
+++ BotTest/src/org/usfirst/frc0/BotTest/commands/ElevatorDownC.java
@@ -41,6 +41,7 @@ public class ElevatorDownC extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.elevator.elevatorMove(-0.25);
     }
 
     // Make this return true when this Command no longer needs to run execute()
