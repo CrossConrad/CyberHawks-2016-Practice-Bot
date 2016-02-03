89b4a89875996ed8f113f9c7460deadb41bc66cb
Rohan Vasavada <rohanvasavada@gmail.com>
2016-02-02 18:24:50 -0600

    Fixed un error

--- BotTest/src/org/usfirst/frc0/BotTest/commands/ClawDownC.java
+++ BotTest/src/org/usfirst/frc0/BotTest/commands/ClawDownC.java
@@ -55,6 +55,7 @@ public class ClawDownC extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.claw.clawMoveStop();
     }
 
     // Called when another command which requires one or more of the same
