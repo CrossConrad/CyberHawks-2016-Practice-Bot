e06fab0be5e26369358a997f58068f38c7aba4b3
Rohan Vasavada <rohanvasavada@gmail.com>
2016-02-05 16:31:04 -0600

    Fixed the GitHub issue - Just sync this version

--- BotTest/src/org/usfirst/frc0/BotTest/commands/Drive.java
+++ BotTest/src/org/usfirst/frc0/BotTest/commands/Drive.java
@@ -30,7 +30,6 @@ public class Drive extends Command {
     protected void initialize() {
     	}
     protected void execute() {
-    	float finalSpeed;
     	Robot.chasis.move(Robot.oi.getRightSpeed(), Robot.oi.getLeftSpeed());
     	if (Robot.chasis.stafeSolenoid.get() != DoubleSolenoid.Value.kForward) {
     		Robot.chasis.chasisStrafe(Robot.oi.rightJoy.getX());
