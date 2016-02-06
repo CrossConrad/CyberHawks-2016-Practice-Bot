a23d3e01d74409b24b1d40e56e5779fbc6fb51da
Rohan Vasavada <rohanvasavada@gmail.com>
2016-02-06 12:42:37 -0600

    Added Constants File

---
+++ BotTest/src/org/usfirst/frc0/BotTest/Constants.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc0.BotTest;
+
+public class Constants {
+	// Port Constants
+	public static final int CHASSSIS_RIGHT_MOTOR = 0;
+	public static final int CHASIS_LEFT_MOTOR = 0;
+	public static final int CHASSIS_STRAFE_MOTOR = 0;
+	
+	public static final int CHASSIS_SOLENOID_A = 0;
+	public static final int CHASSIS_SOLENOID_B = 0;
+	public static final int CHASSIS_SOLENOID_C = 0;
+	
+	public static final int CLAW_VERTICAL = 0;
+	public static final int CLAW_PIVOT = 0;
+	
+	public static final int CLAW_PIVOT_POSITION_A = 0;
+	public static final int CLAW_PIVOT_POSITION_B = 0;
+	public static final int CLAW_PIVOT_POSITION_C = 0;
+	
+	public static final int CLAW_BOTTOM_LIMIT = 0;
+	public static final int CLAW_TOP_LIMIT = 0;
+	
+	public static final int CLAW_SOLENOID_A = 0;
+	public static final int CLAW_SOLENOID_B = 0;
+	public static final int CLAW_SOLENOID_C = 0;
+	
+	public static final int ELEVATOR_VERTICAL = 0;
+	
+	public static final int ELEVATOR_HOOK_HEIGHT_A = 0;
+	public static final int ELEVATOR_HOOK_HEIGHT_B = 0;
+	public static final int ELEVATOR_HOOK_HEIGHT_C = 0;
+	
+}
