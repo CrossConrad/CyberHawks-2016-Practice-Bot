6fbec6975be55a9e4f5a8cb427634b668f3abaf3
CrossConrad <cbc12conrad@gmail.com>
2016-02-06 13:12:17 -0600

    Set port values for constants

--- BotTest/src/org/usfirst/frc0/BotTest/Constants.java
+++ BotTest/src/org/usfirst/frc0/BotTest/Constants.java
@@ -2,31 +2,31 @@ package org.usfirst.frc0.BotTest;
 
 public class Constants {
 	// Port Constants
-	public static final int CHASSSIS_RIGHT_MOTOR = 0;
+	public static final int CHASSSIS_RIGHT_MOTOR = 1;
 	public static final int CHASIS_LEFT_MOTOR = 0;
-	public static final int CHASSIS_STRAFE_MOTOR = 0;
+	public static final int CHASSIS_STRAFE_MOTOR = 2;
 	
 	public static final int CHASSIS_SOLENOID_A = 0;
-	public static final int CHASSIS_SOLENOID_B = 0;
-	public static final int CHASSIS_SOLENOID_C = 0;
+	public static final int CHASSIS_SOLENOID_B = 2;
+	public static final int CHASSIS_SOLENOID_C = 3;
 	
-	public static final int CLAW_VERTICAL = 0;
-	public static final int CLAW_PIVOT = 0;
+	public static final int CLAW_VERTICAL = 4;
+	public static final int CLAW_PIVOT = 5;
 	
 	public static final int CLAW_PIVOT_POSITION_A = 0;
 	public static final int CLAW_PIVOT_POSITION_B = 0;
 	public static final int CLAW_PIVOT_POSITION_C = 0;
 	
-	public static final int CLAW_BOTTOM_LIMIT = 0;
+	public static final int CLAW_BOTTOM_LIMIT = 1;
 	public static final int CLAW_TOP_LIMIT = 0;
 	
 	public static final int CLAW_SOLENOID_A = 0;
-	public static final int CLAW_SOLENOID_B = 0;
-	public static final int CLAW_SOLENOID_C = 0;
+	public static final int CLAW_SOLENOID_B = 4;
+	public static final int CLAW_SOLENOID_C = 5;
 	
-	public static final int ELEVATOR_VERTICAL = 0;
+	public static final int ELEVATOR_VERTICAL = 3;
 	
-	public static final int ELEVATOR_HOOK_HEIGHT_A = 0;
+	public static final int ELEVATOR_HOOK_HEIGHT_A = 2;
 	public static final int ELEVATOR_HOOK_HEIGHT_B = 0;
 	public static final int ELEVATOR_HOOK_HEIGHT_C = 0;
 	
